7fb96d3ac88a6013528536a3aba54b004d3cdfb7
Niall Burdon <n.burdon@protonmail.com>
2022-06-01 16:02:24 +0200

    test

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-  );
+
+ReactDOM.render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
