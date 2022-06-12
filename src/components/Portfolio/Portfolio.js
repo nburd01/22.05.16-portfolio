a5c5f819a335757076658db23fb93a8225ad4df9
Niall Burdon <n.burdon@protonmail.com>
2022-06-12 20:34:34 +0200

    sticky section checkpoint save

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,6 +4,8 @@ import './Portfolio.css'
 export default function Portfolio() {
   return (
     <div className="container-large portfolio" id="portfolio">
+
+      <div className="left">
         <div className="title-div">
             <span>
                 <h3>Portfolio</h3>
@@ -11,6 +13,18 @@ export default function Portfolio() {
         </div>
         <div className="paragraph-div">
             <p>I also like to get funky! Here are some projects that I've done in the past</p>
+        </div>
+      </div>
+      <div className="right">
+        <div className="cards">
+            <div className="card">
+              <a href="https://google.com"target="_blank" rel="noreferrer">
+                <h2>
+                  S.C. Le Rheu
+                </h2>
+                <p>Création de site, UX, UI</p>
+              </a>
+            </div>
         </div>
         <div className="cards">
             <div className="card">
@@ -22,6 +36,77 @@ export default function Portfolio() {
               </a>
             </div>
         </div>
+        <div className="cards">
+            <div className="card">
+              <a href="https://google.com"target="_blank" rel="noreferrer">
+                <h2>
+                  S.C. Le Rheu
+                </h2>
+                <p>Création de site, UX, UI</p>
+              </a>
+            </div>
+        </div>
+        <div className="cards">
+            <div className="card">
+              <a href="https://google.com"target="_blank" rel="noreferrer">
+                <h2>
+                  S.C. Le Rheu
+                </h2>
+                <p>Création de site, UX, UI</p>
+              </a>
+            </div>
+        </div>
+        <div className="cards">
+            <div className="card">
+              <a href="https://google.com"target="_blank" rel="noreferrer">
+                <h2>
+                  S.C. Le Rheu
+                </h2>
+                <p>Création de site, UX, UI</p>
+              </a>
+            </div>
+        </div>
+        <div className="cards">
+            <div className="card">
+              <a href="https://google.com"target="_blank" rel="noreferrer">
+                <h2>
+                  S.C. Le Rheu
+                </h2>
+                <p>Création de site, UX, UI</p>
+              </a>
+            </div>
+        </div>
+        <div className="cards">
+            <div className="card">
+              <a href="https://google.com"target="_blank" rel="noreferrer">
+                <h2>
+                  S.C. Le Rheu
+                </h2>
+                <p>Création de site, UX, UI</p>
+              </a>
+            </div>
+        </div>
+        <div className="cards">
+            <div className="card">
+              <a href="https://google.com"target="_blank" rel="noreferrer">
+                <h2>
+                  S.C. Le Rheu
+                </h2>
+                <p>Création de site, UX, UI</p>
+              </a>
+            </div>
+        </div>
+        <div className="cards">
+            <div className="card">
+              <a href="https://google.com"target="_blank" rel="noreferrer">
+                <h2>
+                  S.C. Le Rheu
+                </h2>
+                <p>Création de site, UX, UI</p>
+              </a>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
