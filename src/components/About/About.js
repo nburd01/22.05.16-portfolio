a662bce37dc518bb02b834f328ad72aa7ee0d724
Niall Burdon <n.burdon@protonmail.com>
2022-06-15 23:28:26 +0200

    about btn added, needs to be linked

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,12 +22,14 @@ export default function About() {
             
             <div className="description-div">
               <div className="description-content">
-                
                 <div className="description">
-                    <h2>Niall Burdon</h2>
+                        <h2>Niall Burdon</h2>
+                  <p>Hi! I'm Niall, a web developer based in Rennes, France. I am very passionate about my job and hope to make a change someday with those skills. Furthermore I am a team player who also used to be a high level athlete and coach so teamwork and organizational skills are my second nature. I thoroughly enjoy problem solving, challenges, creating and learning on the daily.  </p>
+                  <p>My daily tech stack mainly consist of ReactJS, Ruby on Rails and REST APIs, but see the extended list below for more. To keep in touch or contact me go check out my social media accounts, where I'm most active and would love to have a chat!</p>
                 </div>
-                <p>Hi! I'm Niall, a web developer based in Rennes, France. I am very passionate about my job and hope to make a change someday with those skills. Furthermore I am a team player who also used to be a high level athlete and coach so teamwork and organizational skills are my second nature. I thoroughly enjoy problem solving, challenges, creating and learning on the daily.  </p>
-                <p>My daily tech stack mainly consist of ReactJS, Ruby on Rails and REST APIs, but see the extended list below for more. To keep in touch or contact me go check out my social media accounts, where I'm most active and would love to have a chat!</p>
+                <div className="btn-div">
+                  <button>Build my project</button>
+                </div>
               </div>
             </div>
 
