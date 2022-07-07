133375ba558b8e400b0ef0451f54714c344cacd4
Niall Burdon <n.burdon@protonmail.com>
2022-07-07 22:48:17 +0200

    colour linked to local storage

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style/index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import * as themes from './theme/schema.json';
+import { setToLS } from './utils/storage';
 
-
+const Index = () => {
+  setToLS('all-themes', themes.default);
+  return(
+    <App />
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+
+    <Index />
+  ,
   document.getElementById('root')
   );
